f46c79ec0bc3aeb6ef090a02fee014ffe2fdeab1
Pritesh Kumar <pritesh@codeaurora.org>
2016-04-29 18:41:32 -0700

    Enable qc-skia
    Enable qc-skia hooks.
    
    Change-Id: I1d378847d0a1ceb053225e98b1dc96d3dd250d96
    Signed-off-by: Joe Maples <joe@frap129.org>

--- Android.mk
+++ Android.mk
@@ -840,7 +840,7 @@ LOCAL_SHARED_LIBRARIES := \
         libcutils
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-#        LOCAL_WHOLE_STATIC_LIBRARIES += libqc-skia
+        LOCAL_WHOLE_STATIC_LIBRARIES += libqc-skia
 endif
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
