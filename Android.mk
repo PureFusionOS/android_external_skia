34ae92fefde44ff908fa15cb41b13439f141a8f7
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-01-04 06:58:04 +0100

    Use FDO

--- Android.mk
+++ Android.mk
@@ -43,7 +43,7 @@ LOCAL_PATH:= $(call my-dir)
 ###############################################################################
 
 include $(CLEAR_VARS)
-#LOCAL_FDO_SUPPORT := true
+LOCAL_FDO_SUPPORT := true
 # This should be the last -Oxxx specified in LOCAL_CFLAGS
 LOCAL_CFLAGS += -O3
 
